10153fc60a331a763cbaf87781e415991da4c481
Katelyn Dinkgrave <katelyn.dinkgrave@gmail.com>
2015-11-05 15:24:46 -0600

    a lot more styling, starting to add new recipe form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -28,28 +28,3 @@ get "/logout" do
     redirect "/"
   end
 end
-
-get "/users/new" do
-  erb :"users/new"
-end
-
-get "/users/:id" do
-  @user = User.find_by(id: params[:id])
-  if @user
-    erb :"users/profile"
-  else
-    erb :"users/no-such-user"
-  end
-end
-
-
-post "/users" do
-  user = User.new(params[:user])
-  if user.save
-    session[:id] = user.id
-    redirect "/users/#{user.id}"
-  else
-    @errors = user.errors.full_messages
-    erb :"users/new"
-  end
-end
