91f3060a47bb12dbcde9bfb61a01c59b195c4c54
Katelyn Dinkgrave <katelyn.dinkgrave@gmail.com>
2015-11-05 16:43:22 -0600

    work on creating new recipe from form

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,3 +1,17 @@
 get "/recipes/new" do
   erb :"recipes/new"
 end
+
+post "/recipes" do
+  if request.xhr?
+    collection_of_ingredients = params[:ingredients]
+
+    collection_of_ingredients.each do |ingredient|
+      components = ingredient.partition(/\A\d?\s?\W?\d?\W?\d?/)
+      amount = components[1].strip!
+      remaining_elements = components.split(" ")
+      unit_of_measurement = remaining_elements.shift
+      food_item = remaining_elements.join(" ")
+    end
+  end
+end
