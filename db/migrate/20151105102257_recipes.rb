049709fbf8382fdce84714376c8cba472cd6f03f
Katelyn Dinkgrave <katelyn.dinkgrave@gmail.com>
2015-11-05 11:03:18 -0600

    fill out recipes table with constraints

--- db/migrate/20151105102257_recipes.rb
+++ db/migrate/20151105102257_recipes.rb
@@ -1,4 +1,11 @@
 class Recipes < ActiveRecord::Migration
   def change
+    create_table :recipe do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.text :steps, null: false
+
+      t.timestamps null: false
+    end
   end
 end
