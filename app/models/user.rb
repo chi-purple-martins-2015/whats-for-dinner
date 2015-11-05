41a7b923c70a9e755f8ae384030515dcc8a831c0
Katelyn Dinkgrave <katelyn.dinkgrave@gmail.com>
2015-11-05 10:12:00 -0600

    add validations and password methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  # has_many :recipes
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, {presence: true, uniqueness: true}
+  validates :email, {presence: true, uniqueness: true}
+  validates :hashed_password, presence: true
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
 end
