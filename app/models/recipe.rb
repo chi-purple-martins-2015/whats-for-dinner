b30628e1647424bd9bb0a1d78646b51dcab6a6c5
Katelyn Dinkgrave <katelyn.dinkgrave@gmail.com>
2015-11-05 11:29:25 -0600

    add associations for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ActiveRecord::Base
+  belongs_to :creator, class_name: "User"
+  
   has_many :ingredients
+  has_many :food_items, through: :ingredients
 
   validates :name, presence: true
   validates :description, presence: true
--- db/migrate/20151105102257_recipes.rb
+++ db/migrate/20151105102257_recipes.rb
@@ -1,6 +1,7 @@
 class Recipes < ActiveRecord::Migration
   def change
     create_table :recipe do |t|
+      t.belongs_to :creator, index: true
       t.string :name, null: false
       t.text :description, null: false
       t.text :steps, null: false
